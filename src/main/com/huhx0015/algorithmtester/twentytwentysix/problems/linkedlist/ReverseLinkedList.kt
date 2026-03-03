0073adee959b1e37bc5a2c12bd17042b573ad3fc
huhx0015 <mhuh@yoonhuh.com>
2026-03-02 20:17:33 -0800

    Add iterative version of ReverseLinked list.
    Add iterative version of ReverseLinked list.

--- src/main/com/huhx0015/algorithmtester/twentytwentysix/problems/linkedlist/ReverseLinkedList.kt
+++ src/main/com/huhx0015/algorithmtester/twentytwentysix/problems/linkedlist/ReverseLinkedList.kt
@@ -1,7 +1,6 @@
 package com.huhx0015.algorithmtester.twentytwentysix.problems.linkedlist
 
 import com.huhx0015.algorithmtester.twentytwentysix.datastructures.SinglyLinkedList.ListNode
-import com.huhx0015.algorithmtester.twentytwentysix.problems.arrays.RemoveElement.removeElement
 
 /**
  * NeetCode.io
@@ -25,10 +24,6 @@ import com.huhx0015.algorithmtester.twentytwentysix.problems.arrays.RemoveElemen
  *
  * 0 <= The length of the list <= 1000.
  * -1000 <= Node.val <= 1000
- *
- * Complexity:
- * Time: O(n) - each node visited once
- * Space: O(n) - recursion stack (n calls)
  */
 object ReverseLinkedList {
 
@@ -43,8 +38,10 @@ object ReverseLinkedList {
             )
         )
         println("INPUT 1: The head ListNode value of the list before reversing is: ${inputLinkedList1.value}")
-        val reversedList1 = reverseList(head = inputLinkedList1)
-        println("INPUT 1: The new head ListNode value of the list after reversing is: ${reversedList1?.value}")
+        val reversedListRecursive1 = reverseListRecursive(head = inputLinkedList1)
+        println("INPUT 1: The new head ListNode value of the list after reversing recursively is: ${reversedListRecursive1?.value}")
+        val reversedListIterative1 = reverseListIterative(head = inputLinkedList1)
+        println("INPUT 1: The new head ListNode value of the list after reversing iteratively is: ${reversedListIterative1?.value}")
 
         val inputLinkedList2 = ListNode(value = 0,
             next = ListNode(value = 1,
@@ -56,17 +53,65 @@ object ReverseLinkedList {
             )
         )
         println("INPUT 2: The head ListNode value of the list before reversing is: ${inputLinkedList2.value}")
-        val reversedList2 = reverseList(head = inputLinkedList2)
-        println("INPUT 2: The new head ListNode value of the list after reversing is: ${reversedList2?.value}")
+        val reversedListRecursive2 = reverseListRecursive(head = inputLinkedList2)
+        println("INPUT 2: The new head ListNode value of the list after reversing recursively is: ${reversedListRecursive2?.value}")
+        val reversedListIterative2 = reverseListIterative(head = inputLinkedList2)
+        println("INPUT 1: The new head ListNode value of the list after reversing iteratively is: ${reversedListIterative2?.value}")
     }
 
-    fun reverseList(head: ListNode?): ListNode? {
+    // ITERATIVE SOLUTION:
+    // Intuition
+    // Reversing a linked list iteratively is all about flipping pointers one step at a time.
+    // We walk through the list from left to right, and for each node, we redirect its next pointer to point to the node
+    // behind it.
+    //
+    // To avoid losing track of the rest of the list, we keep three pointers:
+    //
+    // curr → the current node we are processing
+    // prev → the node that should come after curr once reversed
+    // temp → the original next node (so we don't break the chain)
+    // By moving these pointers forward in each step, we gradually reverse the entire list.
+    // When curr becomes null, the list is fully reversed, and prev points to the new head.
+    //
+    // Algorithm
+    // Initialize:
+    // prev = null
+    // curr = head
+    // While curr exists:
+    // Save the next node: temp = curr.next
+    // Reverse the pointer: curr.next = prev
+    // Move prev to curr
+    // Move curr to temp
+    // When the loop ends, prev is the new head of the reversed list.
+    // Return prev.
+    //
+    // Complexity:
+    // Time: O(n) - each node visited once
+    // Space: O(1) - three pointer solution
+    fun reverseListIterative(head: ListNode?): ListNode? {
+        var previousNode: ListNode? = null
+        var currentNode: ListNode? = head // Set the currentNode reference to head.
+
+        while (currentNode != null) {
+            val tempNode = currentNode.next // Set the tempNode reference to the next node after currentNode.
+            currentNode.next = previousNode // Update the next node after currentNode to point to the previous node.
+            previousNode = currentNode // Update previousNode reference to the currentNode.
+            currentNode = tempNode // Update the currentNode reference to the tempNode (currentNode.next)
+        }
+        return previousNode
+    }
+
+    // RECURSIVE SOLUTION:
+    // Complexity:
+    // Time: O(n) - each node visited once
+    // Space: O(n) - recursion stack (n calls)
+    fun reverseListRecursive(head: ListNode?): ListNode? {
         // If the current head node is null or the next node of the head is null, it's either an empty LinkedList or the
         // end of the LinkedList has been reached.
         if (head == null || head.next == null) return head
 
         var newHead: ListNode? = head // New head of the list to return in this function.
-        newHead = reverseList(head = head.next) // Use recursive function to get the last node in the list.
+        newHead = reverseListRecursive(head = head.next) // Use recursive function to get the last node in the list.
         head.next?.next = head
         head.next = null // Updates the original next link is invalidated. Needed to ensure that the recursive loop is broken.
 
--- src/test/com/huhx0015/algorithmtester/twentytwentysix/problems/linkedlist/ReverseLinkedListTest.kt
+++ src/test/com/huhx0015/algorithmtester/twentytwentysix/problems/linkedlist/ReverseLinkedListTest.kt
@@ -1,14 +1,15 @@
 package com.huhx0015.algorithmtester.twentytwentysix.problems.linkedlist
 
 import com.huhx0015.algorithmtester.twentytwentysix.datastructures.SinglyLinkedList.ListNode
-import com.huhx0015.algorithmtester.twentytwentysix.problems.linkedlist.ReverseLinkedList.reverseList
+import com.huhx0015.algorithmtester.twentytwentysix.problems.linkedlist.ReverseLinkedList.reverseListIterative
+import com.huhx0015.algorithmtester.twentytwentysix.problems.linkedlist.ReverseLinkedList.reverseListRecursive
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class ReverseLinkedListTest {
 
     @Test
-    fun testReverseLinkedList() {
+    fun testReverseLinkedListRecursive() {
         val inputLinkedList1 = ListNode(value = 1, next =
             ListNode(value = 2,
                 next = ListNode(value = 3,
@@ -18,7 +19,7 @@ class ReverseLinkedListTest {
                 )
             )
         )
-        val reversedList1 = reverseList(head = inputLinkedList1)
+        val reversedList1 = reverseListRecursive(head = inputLinkedList1)
         assertEquals(1, inputLinkedList1.value)
         assertEquals(4, reversedList1?.value)
 
@@ -31,12 +32,46 @@ class ReverseLinkedListTest {
                 )
             )
         )
-        val reversedList2 = reverseList(head = inputLinkedList2)
+        val reversedList2 = reverseListRecursive(head = inputLinkedList2)
         assertEquals(0, inputLinkedList2.value)
         assertEquals(3, reversedList2?.value)
 
         val inputLinkedList3 = ListNode(value = 64, next = null)
-        val reversedList3 = reverseList(head = inputLinkedList3)
+        val reversedList3 = reverseListRecursive(head = inputLinkedList3)
+        assertEquals(64, inputLinkedList3.value)
+        assertEquals(64, reversedList3?.value)
+    }
+
+    @Test
+    fun testReverseLinkedListIterative() {
+        val inputLinkedList1 = ListNode(value = 1, next =
+            ListNode(value = 2,
+                next = ListNode(value = 3,
+                    next = ListNode(value = 4,
+                        next = null
+                    )
+                )
+            )
+        )
+        val reversedList1 = reverseListIterative(head = inputLinkedList1)
+        assertEquals(1, inputLinkedList1.value)
+        assertEquals(4, reversedList1?.value)
+
+        val inputLinkedList2 = ListNode(value = 0,
+            next = ListNode(value = 1,
+                next = ListNode(value = 2,
+                    next = ListNode(value = 3,
+                        next = null
+                    )
+                )
+            )
+        )
+        val reversedList2 = reverseListIterative(head = inputLinkedList2)
+        assertEquals(0, inputLinkedList2.value)
+        assertEquals(3, reversedList2?.value)
+
+        val inputLinkedList3 = ListNode(value = 64, next = null)
+        val reversedList3 = reverseListRecursive(head = inputLinkedList3)
         assertEquals(64, inputLinkedList3.value)
         assertEquals(64, reversedList3?.value)
     }
