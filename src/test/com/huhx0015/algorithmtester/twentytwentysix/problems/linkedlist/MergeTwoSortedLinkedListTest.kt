5d1d775376c67beddf627eed119af0576e6acc95
huhx0015 <mhuh@yoonhuh.com>
2026-03-03 18:29:51 -0800

    Add test for MergeTwoSortedLinkedListTest
    Add test for MergeTwoSortedLinkedListTest

--- src/test/com/huhx0015/algorithmtester/twentytwentysix/problems/linkedlist/MergeTwoSortedLinkedListTest.kt
+++ src/test/com/huhx0015/algorithmtester/twentytwentysix/problems/linkedlist/MergeTwoSortedLinkedListTest.kt
@@ -1,11 +1,29 @@
 package com.huhx0015.algorithmtester.twentytwentysix.problems.linkedlist
 
+import com.huhx0015.algorithmtester.twentytwentysix.datastructures.SinglyLinkedList.ListNode
+import com.huhx0015.algorithmtester.twentytwentysix.problems.linkedlist.MergeTwoSortedLinkedList.mergeTwoLists
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class MergeTwoSortedLinkedListTest {
 
     @Test
-    funMergeTwoSortedLinkedListTest() {
-
+    fun testMergeTwoSortedLinkedList() {
+        val inputLinkedList1 = ListNode(value = 1,
+            next = ListNode(value = 2,
+                next = ListNode(value = 4,
+                    next = null
+                )
+            )
+        )
+        val inputLinkedList2 = ListNode(value = 1,
+            next = ListNode(value = 3,
+                next = ListNode(value = 5,
+                    next = null
+                )
+            )
+        )
+        val mergedSortedList1 = mergeTwoLists(inputLinkedList1, inputLinkedList2)
+        assertEquals(1, mergedSortedList1?.value)
     }
 }
