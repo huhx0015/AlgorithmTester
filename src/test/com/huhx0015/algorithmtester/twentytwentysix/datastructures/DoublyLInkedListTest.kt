7eca73aa07319c5eb561904814bea71fcaa66756
huhx0015 <mhuh@yoonhuh.com>
2026-03-04 18:26:52 -0800

    Add DoublyLinkedListTest
    Add DoublyLinkedListTest

--- src/test/com/huhx0015/algorithmtester/twentytwentysix/datastructures/DoublyLInkedListTest.kt
+++ src/test/com/huhx0015/algorithmtester/twentytwentysix/datastructures/DoublyLInkedListTest.kt
@@ -14,21 +14,48 @@ class DoublyLInkedListTest {
 
         val testHeadNodeValue2 = 64
         testLinkedList.insertFront(testHeadNodeValue2)
-        assertEquals(testHeadNodeValue1, testLinkedList.head.next?.next?.value)
+        assertEquals(testHeadNodeValue2, testLinkedList.head.next?.value)
     }
 
     @Test
     fun testDoublyLinkedListInsertEnd() {
+        val testLinkedList = DoublyLinkedList()
+        val testTailNodeValue1 = 128
+        testLinkedList.insertEnd(testTailNodeValue1)
+        assertEquals(testTailNodeValue1, testLinkedList.tail.prev?.value)
 
+        val testTailNodeValue2 = 256
+        testLinkedList.insertEnd(testTailNodeValue2)
+        assertEquals(testTailNodeValue2, testLinkedList.tail.prev?.value)
     }
 
     @Test
     fun testDoublyLinkedListRemoveFront() {
+        val testLinkedList = DoublyLinkedList()
+        val testHeadNodeValue1 = 44
+        testLinkedList.insertFront(testHeadNodeValue1)
+        assertEquals(testHeadNodeValue1, testLinkedList.head.next?.value)
 
+        val testHeadNodeValue2 = 88
+        testLinkedList.insertFront(testHeadNodeValue2)
+        assertEquals(testHeadNodeValue2, testLinkedList.head.next?.value)
+
+        testLinkedList.removeFront()
+        assertEquals(testHeadNodeValue1, testLinkedList.head.next?.value)
     }
 
     @Test
     fun testDoublyLinkedListRemoveEnd() {
+        val testLinkedList = DoublyLinkedList()
+        val testTailNodeValue1 = 72
+        testLinkedList.insertEnd(testTailNodeValue1)
+        assertEquals(testTailNodeValue1, testLinkedList.tail.prev?.value)
 
+        val testTailNodeValue2 = 144
+        testLinkedList.insertEnd(testTailNodeValue2)
+        assertEquals(testTailNodeValue2, testLinkedList.tail.prev?.value)
+
+        testLinkedList.removeEnd()
+        assertEquals(testTailNodeValue1, testLinkedList.head.next?.value)
     }
 }
