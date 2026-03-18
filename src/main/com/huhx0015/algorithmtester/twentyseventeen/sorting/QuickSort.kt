c91ab868272e1bf3fe6729f366f606248fb3e4da
huhx0015 <mhuh@yoonhuh.com>
2026-03-17 23:59:32 -0700

    Add executable function in QuickSort
    Add executable function in QuickSort

--- src/main/com/huhx0015/algorithmtester/twentyseventeen/sorting/QuickSort.kt
+++ src/main/com/huhx0015/algorithmtester/twentyseventeen/sorting/QuickSort.kt
@@ -7,10 +7,24 @@ package com.huhx0015.algorithmtester.twentyseventeen.sorting
 
  * Created by Michael Yoon Huh on 6/13/2017.
  */
-class QuickSort {
+object QuickSort {
 
     private var numbers: IntArray? = null
     private var number: Int = 0
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val inputArray1 = intArrayOf(7, 5, 3, 8)
+        val inputArray2 = intArrayOf(45, 72, 3, 4, 59, 100)
+
+        println("QuickSort (IntArray) starting, array is: ${inputArray1.toSet()}")
+        sort(inputArray1)
+        println("---------------------------------------------")
+
+        println("QuickSort (IntArray) starting, array is: ${inputArray2.toSet()}")
+        sort(inputArray2)
+        println("---------------------------------------------")
+    }
 
     fun sort(values: IntArray?) {
 
@@ -21,26 +35,28 @@ class QuickSort {
         this.numbers = values
         number = values.size
         quicksort(0, number - 1)
+
+        println("QuickSort (IntArray) finished sorting, sorted array is: ${numbers?.toSet()}")
     }
 
     private fun quicksort(low: Int, high: Int) {
         var i = low
         var j = high
 
-        // Get the pivot element from the middle of the list
+        // Get the pivot element from the middle of the list.
         val pivot = numbers!![low + (high - low) / 2]
 
-        // Divide into two lists
+        // Divide into two lists.
         while (i <= j) {
 
             // If the current value from the left list is smaller than the pivot
-            // element then get the next element from the left list
+            // element then get the next element from the left list.
             while (numbers!![i] < pivot) {
                 i++
             }
 
             // If the current value from the right list is larger than the pivot
-            // element then get the next element from the right list
+            // element then get the next element from the right list.
             while (numbers!![j] > pivot) {
                 j--
             }
@@ -49,7 +65,7 @@ class QuickSort {
             // the pivot element and if we have found a value in the right list
             // which is smaller than the pivot element then we exchange the
             // values.
-            // As we are done we can increase i and j
+            // As we are done we can increase i and j.
             if (i <= j) {
                 exchange(i, j)
                 i++
@@ -57,7 +73,7 @@ class QuickSort {
             }
         }
 
-        // Recursion
+        // Recursion.
         if (low < j)
             quicksort(low, j)
         if (i < high)
