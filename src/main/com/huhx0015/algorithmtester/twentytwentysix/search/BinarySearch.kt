4e13c96a542e4f5bdfc271d71583451c135276a4
huhx0015 <mhuh@yoonhuh.com>
2026-03-21 23:35:08 -0700

    Added more inputs and problem description for BinarySearch
    Added more inputs and problem description for BinarySearch

--- src/main/com/huhx0015/algorithmtester/twentytwentysix/search/BinarySearch.kt
+++ src/main/com/huhx0015/algorithmtester/twentytwentysix/search/BinarySearch.kt
@@ -1,18 +1,49 @@
 package com.huhx0015.algorithmtester.twentytwentysix.search
 
+/**
+ * Problem: Binary Search
+ * Difficulty: Easy
+ *
+ * You are given an array of distinct integers nums, sorted in ascending order, and an integer target.
+ *
+ * Implement a function to search for target within nums. If it exists, then return its index, otherwise, return -1.
+ *
+ * Your solution must run in O(log n) time.
+ *
+ * Example 1:
+ *
+ * Input: nums = [-1,0,2,4,6,8], target = 4
+ *
+ * Output: 3
+ * Example 2:
+ *
+ * Input: nums = [-1,0,2,4,6,8], target = 3
+ *
+ * Output: -1
+ * Constraints:
+ *
+ * 1 <= nums.length <= 10000.
+ * -10000 < nums[i], target < 10000
+ * All the integers in nums are unique.
+ *
+ */
 object BinarySearch {
 
     @JvmStatic fun main(args: Array<String>) {
-        val inputIntArray = intArrayOf(1, 2, 3, 4, 23, 50, 78, 3000, 5000)
+        val inputIntArray1 = intArrayOf(1, 2, 3, 4, 23, 50, 78, 3000, 5000)
+        val inputIntArray2 = intArrayOf(-1, 0, 2, 4, 6, 8)
         val target1 = 23
         val target2 = 3
         val target3 = 77
         val target4 = 3000
+        val target5 = 4
 
-        binarySearch(arr = inputIntArray, target = target1)
-        binarySearch(arr = inputIntArray, target = target2)
-        binarySearch(arr = inputIntArray, target = target3)
-        binarySearch(arr = inputIntArray, target = target4)
+        binarySearch(arr = inputIntArray1, target = target1)
+        binarySearch(arr = inputIntArray1, target = target2)
+        binarySearch(arr = inputIntArray1, target = target3)
+        binarySearch(arr = inputIntArray1, target = target4)
+        binarySearch(arr = inputIntArray2, target = target2)
+        binarySearch(arr = inputIntArray2, target = target5)
     }
 
     // binarySearch(): Standard binary search algorithm.
