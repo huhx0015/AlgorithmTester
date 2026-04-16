5cea70cf0121b77a5e20d32fc6c4cf2ee39e393b
huhx0015 <mhuh@yoonhuh.com>
2026-04-15 22:08:50 -0700

    Add test for CountingBits.countingBits
    Add test for CountingBits.countingBits

--- src/test/com/huhx0015/algorithmtester/twentytwentysix/problems/bits/CountingBitsTest.kt
+++ src/test/com/huhx0015/algorithmtester/twentytwentysix/problems/bits/CountingBitsTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 class CountingBitsTest {
 
     @Test
-    fun testCountingBits() {
+    fun testCountBits() {
         val result1 = CountingBits.countBits(num = 23)
         Assertions.assertEquals(4, result1)
 
@@ -15,5 +15,11 @@ class CountingBitsTest {
 
         val result3 = CountingBits.countBits(num = 100)
         Assertions.assertEquals(3, result3)
+    }
+
+    @Test
+    fun testCountingBits() {
+        val result = CountingBits.countingBits(n = 4)
+        Assertions.assertEquals(intArrayOf(0, 1, 1, 2, 1).toList(), result.toList())
     }
 }
